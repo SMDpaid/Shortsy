24e95b54f9be123566e31ee86bdc23b2ed9a8212
SMDpaid <140572996+SMDpaid@users.noreply.github.com>
2023-08-16 15:05:35 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -2,44 +2,40 @@
 import os
 from os import getenv, environ
 from dotenv import load_dotenv
-
-
-
 load_dotenv()
-
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '13415474'))
-    API_HASH = str(getenv('API_HASH', '01bb828c0429beeabd6e9e841d026231'))
+    API_ID = int(getenv('API_ID', '13415474')
+    API_HASH = str(getenv('API_HASH', '01bb828c0429beeabd6e9e841d026231')
     BOT_TOKEN = str(getenv('BOT_TOKEN', '6434399232:AAFy94DNlPbOjzQlrMEoKcwjeHWRnJGmxFI'))
-    SESSION_NAME = str(getenv('SESSION_NAME', 'qtvsofficial'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001848305113'))
-    PORT = int(getenv('PORT', 8080))
+    SESSION_NAME = str(getenv('SESSION_NAME', 'qtvsofficial')
+    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60')
+    WORKERS = int(getenv('WORKERS', '4')
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001848305113')
+    PORT = int(getenv('PORT', 8080)
     CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "BOTTOM")
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0')
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200") # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "885675538").split())  
-    NO_PORT = bool(getenv('NO_PORT', False))
+    NO_PORT = bool(getenv('NO_PORT', False)
     SHORTENER_API = str(getenv('SHORTENER_API', '1adcdddaef4dd9f90c9e967971b58bd52daff246'))
-    SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'dalink.in'))
-    FDQN = str(getenv('FDQN', 'https://qtvsofficial.onrender.com'))
+    SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'dalink.in')
+    FDQN = str(getenv('FDQN', 'https://qtvsofficial.onrender.com')
 
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('SMD_Owner'))
+    OWNER_USERNAME = str(getenv('SMD_Owner')
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME', 'qtvsofficial'))
+        APP_NAME = str(getenv('APP_NAME', 'qtvsofficial')
     
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
+    HAS_SSL=bool(getenv('HAS_SSL',False)
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://hillsking1222:kuraman1@kumaran1.d6ahc05.mongodb.net/?retryWrites=true&w=majority"))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://hillsking1222:kuraman1@kumaran1.d6ahc05.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', "-1001989401038"))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
